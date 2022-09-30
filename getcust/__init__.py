49878a62a3c9df9423c5b19daa0c6480414a117f
Michal@SFB2 <michal.marusan@gmail.com>
2022-09-30 14:33:47 +0200

    Initial

---
+++ getcust/__init__.py
@@ -0,0 +1,27 @@
+import logging
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest, customerItem: func.DocumentList) -> str:
+    if not customerItem:
+        logging.warning("Customer item not found")
+    else:
+        logging.info("Found Customer item, id=%s",
+                     customerItem[0]['id'])
+        ret = {
+                    "id": customerItem[0]['id'],
+                    "firstname": customerItem[0]['firstname'],
+                    "lastname": customerItem[0]['lastname'],
+                    "email": customerItem[0]['email'],
+                    "date": customerItem[0]['date1'],
+                    "feat0": customerItem[0]['feat0'],
+                    "feat1": customerItem[0]['feat1'],
+                    "feat2": customerItem[0]['feat2'],
+               }
+        return func.HttpResponse(
+            json.dumps(ret),
+            mimetype="application/json",
+
+        )
+
+    return 'OK'
